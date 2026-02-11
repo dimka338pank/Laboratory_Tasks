7b490156c073be808a37e68180ff1f5d256d7860
dimka338 <23b_pds1@liceum.ztu.edu.ua>
2026-02-11 19:53:19 +0200

    Fix: Lab1 is now a proper subdirectory

---
+++ Lab1/Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            double x, n, a, y1, y2, y3, q;
+            Console.WriteLine("Введіть значення x:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введіть значення n:");
+            n = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введіть значення a:");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            y1 = Math.Abs(Math.Cos(x) + Math.Sin(x));
+            Console.WriteLine($"y1 = {y1:F10}");
+            y2 = Math.Cos(Math.Pow(x, 3));
+            Console.WriteLine($"y2 = {y2:F10}");
+            y3 = Math.Exp(Math.Tan(x) / (n + 1)) + Math.Abs((a + 1) / (x - 1));
+            Console.WriteLine($"y3 = {y3:F10}");
+
+        }
+    }
+}
---
+++ Lab1/Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            //Завдання 1 6.  Ввести три окремих символи. Вивести на екран числовий код кожного символу.
+            char a, b, c;
+            Console.WriteLine("Введіть перший символ :");
+            a = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Введіть другий символ :");
+            b = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Введіть третій символ :");
+            c = Convert.ToChar(Console.ReadLine());
+            Console.Write("Числовий код 1-го символу: ");
+            Console.WriteLine(a + 0);
+            int code_b = (int)b;
+            Console.WriteLine("Числовий код 2-го символу: " + code_b);
+            int code_c = (int)c;
+            Console.Write("Числовий код 3-го символу: " + code_c);
+
+            //Завдання 2 6.  Ввести три окремих рядки тексту. Вивести на екран всі рядки у верхньому регістрі.
+
+            string A, B, C;
+            Console.WriteLine("\nВведіть 1-й рядок: ");
+            A = Console.ReadLine();
+            Console.WriteLine("Введіть 2-й рядок: ");
+            B = Console.ReadLine();
+            Console.WriteLine("Введіть 3-й рядок: ");
+            C = Console.ReadLine();
+            Console.Write(A.ToUpper() + ". " + B.ToUpper() + ". " + C.ToUpper() + ".");
+        }
+    }
+}
