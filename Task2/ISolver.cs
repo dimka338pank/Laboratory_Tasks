f6fd9421b5e860dc33c2b55ab643520d9cb93de1
dimka338 <23b_pds1@liceum.ztu.edu.ua>
2026-03-03 17:27:21 +0200

    Реалізовано пошук коренів через абстрактні класи та інтерфейси

--- Task2/ISolver.cs
+++ Task2/ISolver.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using static Task2.InterfaceClass;
 
 namespace Task2
 {
     public class ISolver
     {
-        public void Solve(IFunction func, double start, double end, double step)
+        public void Solve(Inrterface func, double start, double end, double step)
         {
             Console.WriteLine($"Пошук коренів на [{start}, {end}] з кроком {step}: ");
             bool found = false;
---
+++ Task2/Inrterface.cs
@@ -0,0 +1,15 @@
+﻿namespace Task2
+{
+    public interface Inrterface
+    {
+        double IFindRoot(double x);
+    }
+
+    public class QuadraticFunction : Inrterface
+    {
+        public double IFindRoot(double x)
+        {
+            return x * x - 8;
+        }
+    }
+}
