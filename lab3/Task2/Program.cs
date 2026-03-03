5ae55f01f29bc21c61a980c46817b9e32dc8aa05
dimka338 <23b_pds1@liceum.ztu.edu.ua>
2026-03-03 23:37:15 +0200

    Фінальна структура папок

---
+++ lab3/Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            SquareSolver solver = new SquareSolver();
+            double x1, x2;
+            Console.WriteLine("Рівнняня 1 : x^2 - 3x + 2 = 0");
+            int count = solver.SolverWithOut(1, -3, 2, out x1, out x2);
+            if( count == -1) Console.WriteLine("Коренів безліч");
+            if (count > 0)
+            {
+                Console.WriteLine($"x1 = {x1}");
+                Console.WriteLine($"Кількість коренів: {count}");
+            }
+            if (count == 2)
+            {
+                Console.WriteLine($"x2 = {x2}");
+                Console.WriteLine($"Кількість коренів: {count}");
+            }
+
+
+            Console.WriteLine("\nРівнняня 2 : x^2 - 5x + 6 = 0");
+            StructResults results2 = solver.SolveWithStruct(1, -5, 6);
+            Console.WriteLine($"Кількість коренів: {results2.AmountOfRoots}\nx1: {results2.x1}\nx2: {results2.x2}");
+
+
+            Console.WriteLine("\nРівнняня 3 : x^2 - 9x + 20 = 0");
+            (int count, double x1, double x2) result3 = solver.SolveWithTuple(1, -9, 20);
+            Console.WriteLine($"Кількість коренів: {result3.count}");
+            Console.WriteLine($"Перший корінь: {result3.x1}");
+            Console.WriteLine($"Другий корінь: {result3.x2}");
+        }
+
+        
+    }
+}
---
+++ lab3/Task3/Complex.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Task3
+{
+    public class Complex
+    {
+        public double Real { get; set; }
+        public double Imaginary { get; set; }
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            return new Complex { Real = c1.Real + c2.Real, Imaginary = c1.Imaginary + c2.Imaginary };
+        }
+
+        public static Complex operator -(Complex c1, Complex c2)
+        {
+            return new Complex { Real = c1.Real - c2.Real, Imaginary = c1.Imaginary - c2.Imaginary };
+        }
+
+        public static Complex operator *(Complex c1, Complex c2)
+        {
+            return new Complex
+            {
+                Real = c1.Real * c2.Real - c1.Imaginary * c2.Imaginary,
+                Imaginary = c1.Real * c2.Imaginary + c1.Imaginary * c2.Real
+            };
+        }
+
+        public static Complex operator /(Complex c1, Complex c2)
+        {
+            double znamennik = c2.Real * c2.Real + c2.Imaginary * c2.Imaginary;
+            return new Complex
+            {
+                Real = (c1.Real * c2.Real + c1.Imaginary * c2.Imaginary) / znamennik,
+                Imaginary = (c1.Imaginary * c2.Real - c1.Real * c2.Imaginary) / znamennik
+            };
+        }
+
+        public static implicit operator string(Complex c)
+        {
+            return c.ToString();
+        }
+        public override string ToString()
+        {
+            string sign;
+            if (Imaginary >= 0)
+                sign = "+";
+            else       
+                sign = "-";
+            return $"{Real} {sign} {Math.Abs(Imaginary)}i";
+        }
+    }
+}
